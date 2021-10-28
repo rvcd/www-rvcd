e82a2fb7b710e7cc2694e02da4c97ddc52a6de23
Dwight VanTuyl <dvantuyl@gmail.com>
2021-10-28 18:43:14 -0400

    Index page hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,19 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}'],
+  mode: "jit",
+  purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}"],
+  theme: {
+    extend: {
+      colors: {
+        wine: "#941926",
+        cobalt: "#223e4c",
+        folage: "#559c3b",
+        sky: "#56a5b9",
+        mustard: "#d7972a",
+      },
+      fontFamily: {
+        heading: ["Prata"],
+        secondary: ["Glacial Indifference"],
+      },
+    },
+  },
 };
