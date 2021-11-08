d0c3c4448f240c32118f5cc2643f6f587ccba679
Dwight VanTuyl <dvantuyl@gmail.com>
2021-11-07 19:37:09 -0800

    InfoBtn | Fix mobile layout

--- src/components/InfoBtn.svelte
+++ src/components/InfoBtn.svelte
@@ -15,12 +15,13 @@
 {#if isOpen}
 	<div class="fixed top-0 left-0 w-full h-screen bg-gray-900 bg-opacity-90 z-40" transition:fade="{{duration: 100}}">
 		<div class="stack h-full">
-			<img src={image_url} class="sm:h-full w-full place-self-center self-center object-contain max-w-6xl" alt={title}/>
+			<img src={image_url} class="hidden sm:block sm:h-full w-full place-self-center self-center object-contain max-w-6xl" alt={title}/>
 			<button on:click={() => isOpen = false} class="place-self-end opacity-50 hover:opacity-80 absolute top-5 right-5">
 				<svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24" width="30px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M14.59 8L12 10.59 9.41 8 8 9.41 10.59 12 8 14.59 9.41 16 12 13.41 14.59 16 16 14.59 13.41 12 16 9.41 14.59 8zM12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>			
 			</button>
-			<div class="place-self-end bg-gradient-to-t from-black w-full">
-				<div class="main w-full flex justify-between items-center">
+			<div class="place-self-start sm:place-self-end sm:bg-gradient-to-t from-black w-full">
+				<img src={image_url} class="mt-20 sm:hidden sm:h-full w-full object-contain max-w-6xl" alt={title}/>
+				<div class="main w-full flex justify-between items-end sm:items-center">
 					<div class="text-gray-300">
 						<h2 class="text-xl">{title}</h2>
 						<p class="text-lg"
