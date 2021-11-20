51a257eadfab03895e6b6ff878eec51d67afc4d7
Dwight VanTuyl <dvantuyl@gmail.com>
2021-11-20 09:21:00 -0800

    Update astro to 0.21.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,13 @@
 // VSCode and other TypeScript-enabled text editors will provide auto-completion,
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
+import { imagetools } from 'vite-imagetools';
 
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
-  // Enable Tailwind by telling Astro where your Tailwind config file lives.
-  devOptions: {
-    tailwindConfig: './tailwind.config.js',
-  },
   // Enable the Preact renderer to support Preact JSX components.
   renderers: ['@astrojs/renderer-svelte'],
+  vite: {
+    plugins: [imagetools()],
+  },
 });
