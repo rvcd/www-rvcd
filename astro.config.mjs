6a59efcc1cb89877a90fe7f0d57affdf3d27be1a
Dwight VanTuyl <dvantuyl@gmail.com>
2021-12-10 09:55:36 -0800

    Update javascript libs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,9 @@
 // VSCode and other TypeScript-enabled text editors will provide auto-completion,
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
-import { imagetools } from 'vite-imagetools';
 
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Enable the Preact renderer to support Preact JSX components.
   renderers: ['@astrojs/renderer-svelte'],
-  vite: {
-    plugins: [imagetools()],
-  },
 });
