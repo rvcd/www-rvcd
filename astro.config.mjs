33b335b2a77842bfb1cf7466a287e9cd26a5cfe9
Dwight Van Tuyl <dvantuyl@gmail.com>
2025-03-09 16:20:49 -0700

    setup for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rvcd.github.io',
   vite: {
     plugins: [tailwindcss()]
   },
