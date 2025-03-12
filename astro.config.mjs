003633c2e871ba46c9c6bb2fd405be2c001cdeb9
Dwight Van Tuyl <dvantuyl@gmail.com>
2025-03-11 17:32:58 -0700

    Setup github pages custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rvcd.github.io',
-  base: '/www-rvcd',
+  site: 'https://rainiervalleycreativedistrict.com',
   vite: {
     plugins: [tailwindcss()]
   },
