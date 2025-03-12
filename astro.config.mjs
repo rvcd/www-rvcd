2973f0bde61234ff3f1759081ef7950eb3685e85
Dwight Van Tuyl <dvantuyl@gmail.com>
2025-03-11 21:07:54 -0700

    setup netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,21 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rainiervalleycreativedistrict.com',
+
   vite: {
     plugins: [tailwindcss()]
   },
 
   integrations: [svelte()],
+
   image: {
     domains: ['ik.imagekit.io'],
-  }
+  },
+
+  adapter: netlify()
 });
